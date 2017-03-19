e5d63af9758d5e5979f93c2b808544ff067d767e
Ales Komarek <ales.komarek@newt.cz>
2017-03-19 20:49:37 +0100

    better config file

--- robophery/files/robophery_conf.py
+++ robophery/files/robophery_conf.py
@@ -1,9 +1,11 @@
 {%- from "robophery/map.jinja" import server with context %}
 
 CONF = {
+    'name': '{{ server.name }}',
     'log_level': '{{ server.log_level }}',
     'log_handlers': {{ server.log_handlers }},
-    'read_interval': {{ server.read_interval }},
+    'read_interval': {{ server.get('read_interval', 10000) }},
+    'publish_interval': {{ server.get('publish_interval', 60000) }},
     'platform': '{{ server.platform }}',
     'config': {
         'comm': {{ server.communication }},
